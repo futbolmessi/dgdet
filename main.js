a18ab136e37ef449ce6c605b51824c62dc5a3d46
mini messi <98719161+futbolmessi@users.noreply.github.com>
2023-11-28 20:50:30 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var objectDector="";
+var image="";
+var objects=[];
+var status="";
+
+function preload() {
+image=loadImage(""); 
+}
+
+function setup() {
+ canvas=createCanvas(500,500);
+ canvas.center();   
+}
